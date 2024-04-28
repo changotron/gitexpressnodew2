4e24172a51da1a752dac09a4220241814fd58d0e
Michael Lopez <lopez.michael2@gmail.com>
2024-04-28 12:18:26 -0700

    Workshop week 3 NodeJS tasks 1, 2, & 3 completed

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (!req.user.admin) {
+        res.statusCode = 403;
+        ///res.setHeader('Content-Type', 'application/json');
+        res.end('You are not authorized to perform this operation!');
+        //return next(err);
+    }
+    return next();
+}
